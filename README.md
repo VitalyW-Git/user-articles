3f52d4daf409d794b611938311539b38fccff01a
VitalyW-Git <>
2023-11-06 16:21:45 +0300

    test write cookies 01

--- README.md
+++ README.md
@@ -6,13 +6,4 @@
 
 ### `npm run build`
 
-# Create .env, add property
-
-###  MONGO_CONNECT=mongodb+srv://bapec54162:62PbOsvBKn0pc7HL@cluster0.ualn4pg.mongodb.net/publication?retryWrites=true&w=majority
-###  JWT_SECRET=jfnfhgbvb547JDndnfh124dc
-###  TOKEN_KEY=hvfv4f542e8e5d/dcd*\cd125
-###  PORT=8080
-###  URL_PROD=https://app-frontend-puble.vercel.app
-###  URL_DEV=http://localhost:3000
-###  PASSWORD_SALT=10
 
--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,12 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
-app.post('/post/test', (req, res) => {
+app.post('/post1/test', (req, res) => {
     try {
-        res.cookie("test", '123456789djhjdfhsd', {
-            httpOnly: true,
+        res.cookie("post1", '123456789djhjdfhsdpost1', {
+            httpOnly: false,
             maxAge: 2 * 60 * 60 * 1000,
-            secure: process.env.NODE_ENV == 'production',
+            secure: false,
             path: '/',
         });
         return res.status(200).json({
@@ -41,22 +41,22 @@ app.post('/post/test', (req, res) => {
     }
 });
 
-app.get('/get/test', (req, res) => {
+app.post('/post2/test', (req, res) => {
     try {
-        res.cookie("test", '123456789djhjdfhsd', {
+        res.cookie("post2", '123456789djhjdfhsdpost2', {
             httpOnly: true,
             maxAge: 2 * 60 * 60 * 1000,
-            secure: process.env.NODE_ENV == 'production',
+            secure: true,
             path: '/',
         });
         return res.status(200).json({
             success: true,
-            message: `Нет ошибки GET ${process.env.NODE_ENV}`,
+            message: `Нет ошибки post2 ${process.env.NODE_ENV}`,
         });
     } catch (error) {
         return res.status(200).json({
             success: false,
-            message: 'get Ошибка ' + error.message,
+            message: 'post Ошибка ' + error.message,
         });
     }
 });
